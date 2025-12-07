58f5a0473d7e6a3ec67b69afd3830154a7c48248
Saniya Savla <ssaniyasavla@gmail.com>
2025-12-07 18:27:24 -0500

    day 6

--- day6/day6.py
+++ day6/day6.py
@@ -1,19 +1,29 @@
-def calc(op, grid, idx):
-    res = int(grid[0][idx])
-    for row in range(1, len(grid)-1):
-        if op == "+":
-            res += int(grid[row][idx])
-        elif op == "*":
-            res *= int(grid[row][idx])
-    return res
 def load(path="day6/day6.txt"):
-    with open(path, "r") as f:
-        return [list(line.strip().split()) for line in f if line.strip()]
+    with open(path) as f:
+        lines = [line.rstrip("\n") for line in f if line.strip()]
+    return [list(line.ljust(max(len(line) for line in lines))) for line in lines]
 def main():
     grid = load()
-    res = 0
-    for idx in range(len(grid[0])): 
-        res += calc(grid[-1][idx], grid, idx)
+    rows, cols = len(grid), len(grid[0])
+    res, current_nums, curr = 0, [], None
+    for c in range(cols - 1, -1, -1):
+        digits = []
+        for r in range(rows - 1):
+            ch = grid[r][c]
+            if ch.isdigit():
+                digits.append(ch)
+        if digits:
+            num = int("".join(digits))
+            current_nums.append(num)
+        op = grid[-1][c]
+        if op in "+*":
+            if curr is None: curr = op
+            val = current_nums[0]
+            for n in current_nums[1:]:
+                if curr == "+": val += n
+                else: val *= n
+            res += val
+            current_nums, curr = [], None
     print(res)
 if __name__ == "__main__":
     main()
